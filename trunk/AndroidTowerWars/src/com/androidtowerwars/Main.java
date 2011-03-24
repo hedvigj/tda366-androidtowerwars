7182e5c57c4de9966a440600e26ed84e0e3c8298
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-03-24 17:02:42 +0000

    Base AndEngine project, with a turquoise background

--- trunk/AndroidTowerWars/src/com/androidtowerwars/Main.java
+++ trunk/AndroidTowerWars/src/com/androidtowerwars/Main.java
@@ -1,13 +1,42 @@
 package com.androidtowerwars;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
 
-public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+public class Main extends BaseGameActivity {
+
+	private static final int CAMERA_WIDTH = 700;
+    private static final int CAMERA_HEIGHT = 400;
+    private Camera mCamera;
+	
+
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+                new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT),
+                this.mCamera));
+	}
+
+	public void onLoadResources() {
+		
+	}
+
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+		final Scene scene = new Scene(1);
+        scene.setBackground(new ColorBackground(0.08f, 0.6f, 0.8f));
+        return scene;
+	}
+
+	public void onLoadComplete() {
+		
+	}
 }
