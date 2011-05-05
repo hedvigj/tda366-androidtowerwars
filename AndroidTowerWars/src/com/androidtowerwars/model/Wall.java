94b6ec4bbf11168941baa5104d611f8aeeda2419
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-05 21:32:39 +0000

    Wrote some code in wallogic but haven't figured out everything yet

--- AndroidTowerWars/src/com/androidtowerwars/model/Wall.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/Wall.java
@@ -1,12 +1,21 @@
 package com.androidtowerwars.model;
 
+import org.anddev.andengine.entity.primitive.Rectangle;
+
 import com.androidtowerwars.model.World.Team;
 import com.androidtowerwars.view.ObserverSprite;
+import com.androidtowerwars.view.WorldView;
 
 public class Wall implements IWall {
 	private int maxHealth = 10000;
-	private int health = 10000;
+	private int health = 20;
 	private World.Team team;
+	private static Rectangle range;
+	
+	public Wall(float pX, float pY, float range, World.Team team) {
+		this.range = new Rectangle(pX-(range*0.5f)*0.5f, pY-200, range, 400f); // TODO: Ändra range
+		this.team = team;
+	}
 
 
 	public Wall(World.Team team) {
@@ -30,5 +39,9 @@ public class Wall implements IWall {
 	
 	public void setTeam(World.Team team){
 		this.team=team;
+	}
+	
+	public static Rectangle getRange(){
+		return range;
 	}
 }
--- AndroidTowerWars/src/com/androidtowerwars/model/logic/WallLogic.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/logic/WallLogic.java
@@ -1,5 +1,7 @@
 package com.androidtowerwars.model.logic;
 
+import org.anddev.andengine.entity.primitive.Rectangle;
+
 import com.androidtowerwars.GameActivity;
 import com.androidtowerwars.model.Wall;
 import com.androidtowerwars.model.World;
@@ -7,11 +9,26 @@ import com.androidtowerwars.model.World;
 
 public class WallLogic {
 
-	public static void updateCastle(Wall castle, float pSecondsElapsed){
-		World.Team team = castle.getTeam();
+	public synchronized static void updateCastle(Wall wall, float pSecondsElapsed){
+		World.Team team = wall.getTeam();
+		Rectangle range = Wall.getRange();
+		// TODO: Fixa detta, så det fungerar riktigt.
+		for(int n=0;n<GameActivity.instance.soldierController.soldierListMap.get(team.opposite()).size();n++) {
+		if (range.collidesWith(GameActivity.instance.soldierController.soldierSpriteMap.get(GameActivity.instance.soldierController.soldierListMap.get(team)))){
+			SoldierLogic.killSoldier(GameActivity.instance.soldierController.soldierListMap.get(team.opposite()).get(n));
+			damageCastle(wall,team);
+			}
+		}
 	}
 	
-	public static synchronized void damageCastle(Wall castle, World.Team team){
-		castle.setHealth(castle.getHealth()-10);
+	
+	
+	public static synchronized void damageCastle(Wall wall, World.Team team){
+		int health = wall.getHealth();
+		if(health == 0){
+			System.exit(0);
+		}
+		else
+			wall.setHealth(health-10);
 	}
 }
