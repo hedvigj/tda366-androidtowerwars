0861bd6fdb150aa2371a1f8d6f586604159ec198
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-09 19:35:53 +0000

    Two new classes, Ranger and Wizard

--- AndroidTowerWars/src/com/androidtowerwars/model/logic/SoldierLogic.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/logic/SoldierLogic.java
@@ -1,16 +1,26 @@
 package com.androidtowerwars.model.logic;
 
+import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.sprite.Sprite;
 
 import com.androidtowerwars.GameActivity;
 import com.androidtowerwars.controller.SoldierController;
 import com.androidtowerwars.model.ISoldier;
 import com.androidtowerwars.model.Soldier;
+import com.androidtowerwars.model.World;
 
 public class SoldierLogic {
 	
 	public static void updateSoldier(ISoldier soldier, float pSecondsElapsed) {
+		World.Team team = soldier.getTeam();
+		Rectangle range = soldier.getRange();
 		soldier.setPosition(soldier.getX() + soldier.getSpeed() * pSecondsElapsed, soldier.getY());
+		for(int n=0;n<GameActivity.instance.soldierController.soldierListMap.get(team.opposite()).size();n++) {
+			if (range.collidesWith(GameActivity.instance.soldierController.soldierSpriteMap.get(GameActivity.instance.soldierController.soldierListMap.get(team.opposite()).get(n)))) {
+				killSoldier(soldier);
+				break;
+			}
+		}
 	}
 	
 	public static synchronized void killSoldier(final ISoldier soldier) {
