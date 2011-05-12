cf6561ae568f604e6bfc6f377ff180df20d72419
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-12 18:31:57 +0000

    The changes made at todays meeting

--- trunk/AndroidTowerWars/src/com/androidtowerwars/model/logic/ProjectileLogic.java
+++ trunk/AndroidTowerWars/src/com/androidtowerwars/model/logic/ProjectileLogic.java
@@ -5,20 +5,25 @@ import com.androidtowerwars.controller.ProjectileController;
 import com.androidtowerwars.model.IProjectile;
 
 public class ProjectileLogic {
-	
-	public static void updateProjectile(IProjectile projectile, final float pSecondsElapsed) {
-		projectile.setPosition(projectile.getX() + projectile.getDirection().getX() * projectile.getSpeed() * pSecondsElapsed, projectile.getY() + projectile.getDirection().getY() * projectile.getSpeed() * pSecondsElapsed);
-		if (GameActivity.instance.projectileController.projectileSpriteMap.get(projectile).collidesWith(GameActivity.instance.soldierController.soldierSpriteMap.get(projectile.getTarget()))) {
+
+	public static void updateProjectilePosition(IProjectile projectile, final float pSecondsElapsed) {
+		projectile.setPosition(projectile.getX() + projectile.getDirection().getX() * projectile.getSpeed() * pSecondsElapsed,
+		                       projectile.getY() + projectile.getDirection().getY() * projectile.getSpeed() * pSecondsElapsed);
+	}
+	public static void updateProjectileState(IProjectile projectile, final float pSecondsElapsed) {
+		
 			projectile.getTarget().setHealth(projectile.getTarget().getHealth() - projectile.getParent().getDamage());
 			if (projectile.getTarget().getHealth() <= 0) {
 				SoldierLogic.killSoldier(projectile.getTarget());
 				projectile.getParent().increaseKills();
 			}
 			killProjectile(projectile);
-		}
+		
 	}
 	
 	public static synchronized void killProjectile(final IProjectile projectile) {
 		ProjectileController.removeProjectile(projectile, GameActivity.instance.projectileController.projectileListMap.get(projectile.getParent().getTeam()));
 	}
+
+	
 }
