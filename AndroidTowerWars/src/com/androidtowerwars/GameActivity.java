b64110c9949c401626e72d4305facecd329bfe8b
Malin.G.Olofsson@gmail.com <Malin.G.Olofsson@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-17 20:11:37 +0000

    relocated the HUD to TouchView.

--- AndroidTowerWars/src/com/androidtowerwars/GameActivity.java
+++ AndroidTowerWars/src/com/androidtowerwars/GameActivity.java
@@ -48,7 +48,7 @@ import com.androidtowerwars.view.MenuView;
 import com.androidtowerwars.view.TouchView;
 import com.androidtowerwars.view.WorldView;
 
-public class GameActivity extends BaseGameActivity{
+public class GameActivity extends BaseGameActivity {
 
 	// ===========================================================
 	// Constants
@@ -57,7 +57,7 @@ public class GameActivity extends BaseGameActivity{
 	// ===========================================================
 	// Fields
 	// ===========================================================
-	
+
 	public static GameActivity instance = null;
 	private Texture mBackgroundTexture;
 	private Texture mSkeletonTexture;
@@ -76,28 +76,21 @@ public class GameActivity extends BaseGameActivity{
 	public TowerController towerController;
 	public WallController wallController;
 	public TowerTileController towerTileController;
-	
-	
 
-	private Texture mCoinTexture;
-	private TextureRegion mCoinTextureRegion;
 	private Wall goodWall;
 	private Wall badWall;
-	private HUD headUpDisplay;
+
 	private static long timestamp;
-	private Sprite coin;
-	private  ChangeableText goldText;
-	private Font mFont;
-	private Texture mFontTexture;
-	
+
 	private TouchController touchController = new TouchController(this);
 	private TouchView touchView = new TouchView(this);
 	private MenuView menuView = new MenuView(this);
 	private MenuController menuController = new MenuController(this, menuView);
+
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
-	
+
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
 	// ===========================================================
@@ -117,7 +110,7 @@ public class GameActivity extends BaseGameActivity{
 	}
 
 	public void onLoadResources() {
-		
+
 		menuView.loadResources();
 		touchView.loadResources();
 		this.mBackgroundTexture = new Texture(2048, 512,
@@ -130,174 +123,177 @@ public class GameActivity extends BaseGameActivity{
 		this.mSkeletonTextureRegion = TextureRegionFactory.createFromAsset(
 				this.mSkeletonTexture, this, "gfx/skeleton_master_wizard.png",
 				0, 0);
-		this.mArrowTexture = new Texture(64, 64, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mArrowTexture = new Texture(64, 64,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		this.mArrowTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mArrowTexture, this, "gfx/Arrow2.png",
-				0, 0);
+				this.mArrowTexture, this, "gfx/Arrow2.png", 0, 0);
 		this.mTowerTexture = new Texture(256, 256,
 				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		this.mTowerTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mTowerTexture, this, "gfx/tower1.png",
-				0, 0);
-		this.mWallTexture = new Texture(256,256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+				this.mTowerTexture, this, "gfx/tower1.png", 0, 0);
+		this.mWallTexture = new Texture(256, 256,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		this.mWallTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mWallTexture, this, "gfx/wall.png", 0,0);
+				this.mWallTexture, this, "gfx/wall.png", 0, 0);
 		this.mButtonTexture = new Texture(256, 128,
 				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-        
+
 		mButtonTextureRegion = TextureRegionFactory.createFromAsset(
-				mButtonTexture, this, "gfx/GroundTile.png", 0,0);
-		
-		
-		
-		this.mCoinTexture = new Texture(32,32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		this.mCoinTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mCoinTexture, this, "gfx/Coin.png", 0,0);
-		
-		this.mFontTexture = new Texture(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+				mButtonTexture, this, "gfx/GroundTile.png", 0, 0);
 
-        this.mFont = new Font(this.mFontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32, true, Color.WHITE);
-
-        this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
-        this.mEngine.getFontManager().loadFont(this.mFont);
-
-		
-        this.mEngine.getTextureManager().loadTexture(this.mButtonTexture);
+		this.mEngine.getTextureManager().loadTexture(this.mButtonTexture);
 		getEngine().getTextureManager().loadTexture(this.mBackgroundTexture);
 		getEngine().getTextureManager().loadTexture(this.mSkeletonTexture);
 		getEngine().getTextureManager().loadTexture(this.mArrowTexture);
 		getEngine().getTextureManager().loadTexture(this.mTowerTexture);
 		getEngine().getTextureManager().loadTexture(this.mWallTexture);
-		getEngine().getTextureManager().loadTexture(this.mCoinTexture);
-    
-      
 
 	}
 
 	public Scene onLoadScene() {
 		final Scene scene = new Scene(1);
-		this.headUpDisplay = new HUD();
+
 		getEngine().registerUpdateHandler(new FPSLogger());
 		final Sprite background = new Sprite(0, 0,
 				this.mBackgroundTextureRegion);
 		scene.getLastChild().attachChild(background);
-		
-		//TODO:Inte riktigt klart
-		final Sprite gWall = new Sprite(WorldView.MAP_WIDTH - mWallTextureRegion.getWidth(), WorldView.MAP_HEIGHT * 0.3f, mWallTextureRegion);
+
+		// TODO:Inte riktigt klart
+		final Sprite gWall = new Sprite(WorldView.MAP_WIDTH
+				- mWallTextureRegion.getWidth(), WorldView.MAP_HEIGHT * 0.3f,
+				mWallTextureRegion);
 		scene.getLastChild().attachChild(gWall);
-		final Sprite bWall = new Sprite(0, WorldView.MAP_HEIGHT*0.3f, mWallTextureRegion);
+		final Sprite bWall = new Sprite(0, WorldView.MAP_HEIGHT * 0.3f,
+				mWallTextureRegion);
 		scene.getLastChild().attachChild(bWall);
-		goodWall = new Wall(WorldView.MAP_WIDTH, WorldView.MAP_HEIGHT * 0.5f, mWallTextureRegion.getWidth(), Team.GOOD);
-		badWall = new Wall(0, WorldView.MAP_HEIGHT*0.5f, mWallTextureRegion.getWidth(), Team.EVIL);
-		
+		goodWall = new Wall(WorldView.MAP_WIDTH, WorldView.MAP_HEIGHT * 0.5f,
+				mWallTextureRegion.getWidth(), Team.GOOD);
+		badWall = new Wall(0, WorldView.MAP_HEIGHT * 0.5f,
+				mWallTextureRegion.getWidth(), Team.EVIL);
+
 		soldierController = new SoldierController();
 		scene.getLastChild().attachChild(soldierController);
 		projectileController = new ProjectileController();
 		scene.getLastChild().attachChild(projectileController);
-		wallController =  new WallController(goodWall, badWall);
+		wallController = new WallController(goodWall, badWall);
 		scene.getLastChild().attachChild(wallController);
 		towerController = TowerController.getInstance();
 		towerTileController = new TowerTileController();
-		
+
 		touchView.loadScene(scene);
 		menuView.createMenuScene();
-		
+
 		/* Limit scene size */
-		WorldView.getInstance().getCamera().setBounds(0, WorldView.MAP_WIDTH, 0, WorldView.MAP_HEIGHT);
+		WorldView.getInstance().getCamera()
+				.setBounds(0, WorldView.MAP_WIDTH, 0, WorldView.MAP_HEIGHT);
 		WorldView.getInstance().getCamera().setBoundsEnabled(true);
 		/* Center camera */
-		WorldView.getInstance().getCamera().setCenter(WorldView.MAP_WIDTH * 0.88f,
-				WorldView.MAP_HEIGHT * 0.5f);
+		WorldView
+				.getInstance()
+				.getCamera()
+				.setCenter(WorldView.MAP_WIDTH * 0.88f,
+						WorldView.MAP_HEIGHT * 0.5f);
 
 		Scene.IOnSceneTouchListener touchListener = new DragAndZoomController(
 				WorldView.getInstance().getCamera());
 
 		scene.setOnSceneTouchListener(touchListener);
 
-		
-				
 		scene.setOnAreaTouchListener(touchController);
-		
+
 		int tileSpacing = 200;
-		for(int n = 0;n<6;n++) {
-			//good towers
-			ClickButton tempSprite = new ClickButton(WorldView.MAP_WIDTH - (700 + (tileSpacing*n)), WorldView.MAP_HEIGHT * 0.44f , mButtonTextureRegion);
-			TowerTile tempTowerTile = new TowerTile(WorldView.MAP_WIDTH - (700 + (tileSpacing*n)), WorldView.MAP_HEIGHT * 0.44f, mButtonTextureRegion.getWidth(),mButtonTextureRegion.getHeight(), World.Team.GOOD);
-			TowerTileController.towerTileListMap.get(World.Team.GOOD).add(tempTowerTile);
-			TowerTileController.towerTileSpriteMap.put(tempSprite, tempTowerTile);
+		for (int n = 0; n < 6; n++) {
+			// good towers
+			ClickButton tempSprite = new ClickButton(WorldView.MAP_WIDTH
+					- (700 + (tileSpacing * n)), WorldView.MAP_HEIGHT * 0.44f,
+					mButtonTextureRegion);
+			TowerTile tempTowerTile = new TowerTile(WorldView.MAP_WIDTH
+					- (700 + (tileSpacing * n)), WorldView.MAP_HEIGHT * 0.44f,
+					mButtonTextureRegion.getWidth(),
+					mButtonTextureRegion.getHeight(), World.Team.GOOD);
+			TowerTileController.towerTileListMap.get(World.Team.GOOD).add(
+					tempTowerTile);
+			TowerTileController.towerTileSpriteMap.put(tempSprite,
+					tempTowerTile);
 			scene.registerTouchArea(tempSprite);
 			scene.getLastChild().attachChild(tempSprite);
-			
-			tempSprite = new ClickButton(WorldView.MAP_WIDTH - (700 + (tileSpacing*n)), WorldView.MAP_HEIGHT * 0.56f , mButtonTextureRegion);
-			tempTowerTile = new TowerTile(WorldView.MAP_WIDTH - (700 + (tileSpacing*n)), WorldView.MAP_HEIGHT * 0.56f , mButtonTextureRegion.getWidth(), mButtonTextureRegion.getHeight(), World.Team.GOOD);
-			TowerTileController.towerTileListMap.get(World.Team.GOOD).add(tempTowerTile);
-			TowerTileController.towerTileSpriteMap.put(tempSprite, tempTowerTile);
+
+			tempSprite = new ClickButton(WorldView.MAP_WIDTH
+					- (700 + (tileSpacing * n)), WorldView.MAP_HEIGHT * 0.56f,
+					mButtonTextureRegion);
+			tempTowerTile = new TowerTile(WorldView.MAP_WIDTH
+					- (700 + (tileSpacing * n)), WorldView.MAP_HEIGHT * 0.56f,
+					mButtonTextureRegion.getWidth(),
+					mButtonTextureRegion.getHeight(), World.Team.GOOD);
+			TowerTileController.towerTileListMap.get(World.Team.GOOD).add(
+					tempTowerTile);
+			TowerTileController.towerTileSpriteMap.put(tempSprite,
+					tempTowerTile);
 			scene.registerTouchArea(tempSprite);
 			scene.getLastChild().attachChild(tempSprite);
-			
-			//evil towers
-			tempSprite = new ClickButton(500 + (tileSpacing*n), WorldView.MAP_HEIGHT*0.44f, mButtonTextureRegion);
-			tempTowerTile = new TowerTile(500 + (tileSpacing*n), WorldView.MAP_HEIGHT*0.44f, mButtonTextureRegion.getWidth(), mButtonTextureRegion.getHeight(), World.Team.EVIL);
-			TowerTileController.towerTileListMap.get(World.Team.EVIL).add(tempTowerTile);
-			TowerTileController.towerTileSpriteMap.put(tempSprite, tempTowerTile);
+
+			// evil towers
+			tempSprite = new ClickButton(500 + (tileSpacing * n),
+					WorldView.MAP_HEIGHT * 0.44f, mButtonTextureRegion);
+			tempTowerTile = new TowerTile(500 + (tileSpacing * n),
+					WorldView.MAP_HEIGHT * 0.44f,
+					mButtonTextureRegion.getWidth(),
+					mButtonTextureRegion.getHeight(), World.Team.EVIL);
+			TowerTileController.towerTileListMap.get(World.Team.EVIL).add(
+					tempTowerTile);
+			TowerTileController.towerTileSpriteMap.put(tempSprite,
+					tempTowerTile);
 			scene.registerTouchArea(tempSprite);
 			scene.getLastChild().attachChild(tempSprite);
-			
-			tempSprite = new ClickButton(500 + (tileSpacing*n), WorldView.MAP_HEIGHT*0.56f, mButtonTextureRegion);
-			tempTowerTile = new TowerTile(500 + (tileSpacing*n), WorldView.MAP_HEIGHT*0.56f, mButtonTextureRegion.getWidth(), mButtonTextureRegion.getHeight(), World.Team.EVIL);
-			TowerTileController.towerTileListMap.get(World.Team.EVIL).add(tempTowerTile);
-			TowerTileController.towerTileSpriteMap.put(tempSprite, tempTowerTile);
+
+			tempSprite = new ClickButton(500 + (tileSpacing * n),
+					WorldView.MAP_HEIGHT * 0.56f, mButtonTextureRegion);
+			tempTowerTile = new TowerTile(500 + (tileSpacing * n),
+					WorldView.MAP_HEIGHT * 0.56f,
+					mButtonTextureRegion.getWidth(),
+					mButtonTextureRegion.getHeight(), World.Team.EVIL);
+			TowerTileController.towerTileListMap.get(World.Team.EVIL).add(
+					tempTowerTile);
+			TowerTileController.towerTileSpriteMap.put(tempSprite,
+					tempTowerTile);
 			scene.registerTouchArea(tempSprite);
 			scene.getLastChild().attachChild(tempSprite);
 		}
-		
-        
 
-        coin = new Sprite(WorldView.CAMERA_WIDTH-175, 10, mCoinTextureRegion);
-        goldText = new ChangeableText(WorldView.CAMERA_WIDTH-225, 10, this.mFont, "", "XXXXX".length());
+		scene.registerUpdateHandler(new TimerHandler(1 / 20.0f, true,
+				new ITimerCallback() {
 
-       
-       
-        headUpDisplay.getLastChild().attachChild(coin);
-        headUpDisplay.getLastChild().attachChild(goldText);
-        
-        WorldView.getInstance().getCamera().setHUD(this.headUpDisplay);
-        
-        scene.registerUpdateHandler(new TimerHandler(1 / 20.0f, true, new ITimerCallback() {
-            
-            public void onTimePassed(final TimerHandler pTimerHandler) {
-            	 goldText.setText(Integer.toString(Player.getGold()));
-            }
-    }));
+					public void onTimePassed(final TimerHandler pTimerHandler) {
+						TouchView.goldText.setText(Integer.toString(Player
+								.getGold()));
+					}
+				}));
 		return scene;
 	}
 
 	public void onLoadComplete() {
-		Toast.makeText(getApplicationContext(), "In the land of Agarwaen the war has been raging for 30 years.", Toast.LENGTH_LONG).show();
+		Toast.makeText(
+				getApplicationContext(),
+				"In the land of Agarwaen the war has been raging for 30 years.",
+				Toast.LENGTH_LONG).show();
 	}
 
-     
-
-
-     // ===========================================================
-     // Methods
-     // ===========================================================
-		@Override
-	   public boolean onKeyDown(final int pKeyCode, final KeyEvent pEvent) {
-	        if(menuController.onKeyDown(pKeyCode, pEvent)) {    
-	                return true;
-	        } else {
-	                return super.onKeyDown(pKeyCode, pEvent);
-	        }
+	// ===========================================================
+	// Methods
+	// ===========================================================
+	@Override
+	public boolean onKeyDown(final int pKeyCode, final KeyEvent pEvent) {
+		if (menuController.onKeyDown(pKeyCode, pEvent)) {
+			return true;
+		} else {
+			return super.onKeyDown(pKeyCode, pEvent);
+		}
 	}
 
-	
-    public static void setTimestamp(int i) {
-        Log.d("TowerWars", "time" + i);
-        timestamp = 0;
-    }
-
-
+	public static void setTimestamp(int i) {
+		Log.d("TowerWars", "time" + i);
+		timestamp = 0;
+	}
 
 	// ===========================================================
 	// Methods
