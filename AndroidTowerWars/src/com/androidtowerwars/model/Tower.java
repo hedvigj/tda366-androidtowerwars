ca5d9651fb1fc3233d8f483a4d9e4f1d86de3562
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-18 15:41:07 +0000

    Seperated the logic from AI, and put the other in an AI controller

--- AndroidTowerWars/src/com/androidtowerwars/model/Tower.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/Tower.java
@@ -2,9 +2,13 @@ package com.androidtowerwars.model;
 
 
 
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.anddev.andengine.engine.handler.timer.ITimerCallback;
 import org.anddev.andengine.engine.handler.timer.TimerHandler;
 import org.anddev.andengine.entity.primitive.Rectangle;
+import org.anddev.andengine.entity.sprite.Sprite;
 
 import com.androidtowerwars.controller.TowerController;
 import com.androidtowerwars.model.logic.TowerLogic;
@@ -20,6 +24,9 @@ public class Tower implements ITower {
 	private float attack_speed = 0.7f; //seconds delay
 	private int kills = 0;
 	public static int cost = 20;
+	
+	public static ConcurrentHashMap<World.Team, List<ITower>> towerListMap = new ConcurrentHashMap<World.Team, List<ITower>>();
+	public static ConcurrentHashMap<ITower, Sprite> towerSpriteMap = new ConcurrentHashMap<ITower, Sprite>();
 	
 	public Tower(float pX, float pY, float range, World.Team team) {
 		this.range = new Rectangle(pX-(range*0.5f)*0.5f, pY-400, range, 900f); //not centered by width!
--- AndroidTowerWars/src/com/androidtowerwars/model/logic/ArtificialIntelligence.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/logic/ArtificialIntelligence.java
@@ -1,95 +1,58 @@
 package com.androidtowerwars.model.logic;
 
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.Random;
-
-import android.util.Log;
-
-import com.androidtowerwars.controller.SoldierController;
-import com.androidtowerwars.controller.TowerTileController;
 import com.androidtowerwars.model.Player;
-import com.androidtowerwars.model.Tower;
-import com.androidtowerwars.model.TowerTile;
-import com.androidtowerwars.model.World;
-import com.androidtowerwars.view.WorldView;
 
-public class ArtificialIntelligence implements Observer{
-	private static Player evilPlayer;
-	private static World.Team team;
-	private static float updateInterval = 0.5f;
-	private static int nBuiltTowers = 0;
+public class ArtificialIntelligence{
 	
+	public static Player evilPlayer;
 	
-	public ArtificialIntelligence(Player evilPlayer) {
-		this.evilPlayer = evilPlayer;
-		team = evilPlayer.getTeam();
-		evilPlayer.increaseGold(30);
+	public static boolean evilRich(){
+		if(evilPlayer.getGold() > 125){
+		return true;
+		}
+		return false;
+	}
+	
+	public static boolean evilLessRich(){
+		if(evilPlayer.getGold() > 42){
+		return true;
+		}
+		else return false;
 	}
 	
 	
-	public static void updateByTime() {
-		Log.d("TowerWars", "updateByTime  Cash: "+evilPlayer.getGold());
-		if (evilPlayer.getGold() > 125) {
-			if (Math.random() <= 0.3) {
-				buildTower();
-				trainSoldier();
-			}
-			else {
-				trainSoldier();
-			}
+	public static boolean evilSurpriseSmall(){
+		if(Math.random() <= 0.3){
+		return true;
 		}
-		else if (evilPlayer.getGold() > 42){
-			trainSoldier();
-		}
-		
-		if (Math.random() <= 0.2) {
-			trainSoldier();
-		}
-	}
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		Player model = (Player) arg0;
-		
-		if (evilPlayer.getGold() > 39) {
-			if (Math.random() <= 0.25) {
-				buildTower();
-				trainSoldier();
-			}
-			else {
-				trainSoldier();
-			}
-		}
-		else if(evilPlayer.getGold() > 5) {
-			if (Math.random() <= 0.25){
-				trainSoldier();
-			}
-			
-		}
+		else return false;
 	}
 	
-	public static void buildTower() {
-		List<TowerTile> tileList = TowerTile.towerTileListMap.get(team);
-		Random r = new Random();
-		while (nBuiltTowers < tileList.size()) {
-			TowerTile tile = tileList.get(r.nextInt(tileList.size()));
-			if (!tile.isOccupied()) {
-				tile.action();
-				nBuiltTowers++;
-				break;
-			}
+	public static boolean evilSurpriseBig(){
+		if(Math.random() <= 0.2){
+			return true;
 		}
-		return;
+		else return false;
 	}
 	
-	public static void trainSoldier() {
-		SoldierController.createSprite(300, WorldView.MAP_HEIGHT*0.52f, World.Team.EVIL);
-		return;
+	public static boolean evilSurpriseMedium(){
+		if(Math.random() <= 0.2){
+			return true;
+		}
+		else return false;
 	}
 	
-	public static float getUpdateInterval() {
-		return updateInterval;
+	public static boolean evilKindaPoor(){
+		if(evilPlayer.getGold() > 39){
+			return true;
+		}
+		else return false;
 	}
-
+	
+	public static boolean evilPoor(){
+		if(evilPlayer.getGold() > 5){
+			return true;
+		}
+		else return false;
+	}
 }
