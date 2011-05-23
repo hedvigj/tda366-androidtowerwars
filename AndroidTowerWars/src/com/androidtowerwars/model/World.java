691a18903cee0de6972c26d3b3813d12c380de40
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-23 16:28:52 +0000

    addSoldier and getSoldier in World

--- AndroidTowerWars/src/com/androidtowerwars/model/World.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/World.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+public class World {
 
-public class World{
-	
 	public static ConcurrentHashMap<Team, List<TowerTile>> towerTileListMap = new ConcurrentHashMap<Team, List<TowerTile>>();
 	public static ConcurrentHashMap<Team, List<ITower>> towerListMap = new ConcurrentHashMap<Team, List<ITower>>();
-	
+
 	private List<ISoldier> goodSoldier = new ArrayList<ISoldier>();
-	private List<ISoldier> badSoldier = new ArrayList<ISoldier>();
-	
+	private List<ISoldier> evilSoldier = new ArrayList<ISoldier>();
+
 	private static World instance = null;
-	
+
 	public static World getInstance() {
 		if (instance == null) {
-			instance = new World(); 
+			instance = new World();
 		}
 		return instance;
 	}
-}    
 
+	public List<ISoldier> getSoldier(Team team) {
+		if (team == Team.EVIL) {
+			return evilSoldier;
+		} else {
+			return goodSoldier;
+		}
+	}
+
+	public void addSoldier(ISoldier soldier, Team team) {
+		if (team == Team.EVIL) {
+			evilSoldier.add(soldier);
+		} else {
+			goodSoldier.add(soldier);
+		}
+	}
+}
