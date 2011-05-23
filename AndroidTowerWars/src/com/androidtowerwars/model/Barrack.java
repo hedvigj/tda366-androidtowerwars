3cffab5df8cb7d452b8442ea9afaad8b2229d8da
jonsson.hedvig@gmail.com <jonsson.hedvig@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-23 22:12:45 +0000

    Added an interface for Player

--- AndroidTowerWars/src/com/androidtowerwars/model/Barrack.java
+++ AndroidTowerWars/src/com/androidtowerwars/model/Barrack.java
@@ -11,11 +11,11 @@ public class Barrack implements IBarrack{
 	public static ConcurrentHashMap<Team, List<ISoldier>> soldierListMap = new ConcurrentHashMap<Team, List<ISoldier>>();
 	private List<ISoldier> soldierList = new ArrayList<ISoldier>();
 	
-	public List<ISoldier> getSoldier() {
+	public List<ISoldier> getSoldiers() {
 		return soldierList;
 	}
 
-	public void addSoldier(ISoldier soldier) {
+	public void addSoldiers(ISoldier soldier) {
 		soldierList.add(soldier);
 	}
 
---
+++ AndroidTowerWars/src/com/androidtowerwars/model/IPlayer.java
@@ -0,0 +1,15 @@
+package com.androidtowerwars.model;
+
+public interface IPlayer {
+	
+	public int getGold();
+	public Team getTeam();
+	public void setGold(int gold);
+	public void setTeam(Team team);
+	public void decreaseGold(int cost);
+	public void increaseGold(int earnings);
+	public void updateAI();
+	public Barrack getBarrack();
+
+
+}
