c3912a2851fab7c8882ecf14115ba63bd01e8c8b
Mikael666jo@gmail.com <Mikael666jo@gmail.com@9425bc4a-794d-d499-5fcf-171404284fcc>
2011-05-24 00:21:07 +0000

    Added sellTower-ability. Left button does the trick.

--- trunk/AndroidTowerWars/src/com/androidtowerwars/controller/TowerController.java
+++ trunk/AndroidTowerWars/src/com/androidtowerwars/controller/TowerController.java
@@ -9,12 +9,15 @@ import org.anddev.andengine.engine.handler.timer.TimerHandler;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
+import android.util.Log;
+
 import com.androidtowerwars.GameActivity;
 import com.androidtowerwars.model.ITower;
 import com.androidtowerwars.model.MagmaPitTower;
 import com.androidtowerwars.model.Player;
 import com.androidtowerwars.model.Team;
 import com.androidtowerwars.model.Tower;
+import com.androidtowerwars.model.TowerTile;
 import com.androidtowerwars.model.World;
 import com.androidtowerwars.model.logic.TowerLogic;
 import com.androidtowerwars.view.TowerView;
@@ -41,7 +44,7 @@ public class TowerController {//extends Entity {
 		return instance;
 	}
 
-    public static synchronized void createTestTower(float pX, float pY, TextureRegion pTextureRegion, float range, Team team) {
+    public static synchronized Tower createTestTower(float pX, float pY, TextureRegion pTextureRegion, float range, Team team) {
         final Tower tower = new Tower(pX, pY, range, team);
         Sprite sprite = new Sprite(pX, pY, pTextureRegion);
         World.towerListMap.get(team).add(tower);
@@ -59,9 +62,9 @@ public class TowerController {//extends Entity {
                 timerHandlerList.add(timerHandler);
         WorldView.getInstance().registerUpdateHandler(timerHandler);
         World.playerMap.get(team).decreaseGold(tower.getCost());
-        return;
+        return tower;
     }
-    public static synchronized void createMagmaPitTower(float pX, float pY, TextureRegion pTextureRegion, float range, Team team) {
+    public static synchronized Tower createMagmaPitTower(float pX, float pY, TextureRegion pTextureRegion, float range, Team team) {
         final MagmaPitTower tower = new MagmaPitTower(pX, pY, range, team);
         Sprite sprite = new Sprite(pX, pY, pTextureRegion);
         World.towerListMap.get(team).add(tower);
@@ -79,10 +82,25 @@ public class TowerController {//extends Entity {
                 timerHandlerList.add(timerHandler);
         WorldView.getInstance().registerUpdateHandler(timerHandler);
         World.playerMap.get(team).decreaseGold(tower.getCost());
-        return;
+        return tower;
     }
 
-
+    public static synchronized void sellTower(final TowerTile towerTile) {
+        int i = (int) ((int) towerTile.getTower().getCost()*0.75);
+        World.playerMap.get(towerTile.getTeam()).increaseGold(i);
+        removeTower(towerTile);
+    }
+    
+    public static synchronized void removeTower(final TowerTile towerTile) {
+        WorldView.getInstance().runOnUpdateThread(new Runnable() {
+            public void run() {
+                World.towerListMap.get(towerTile.getTeam()).remove(towerTile.getTower());
+                World.towerTileListMap.get(towerTile.getTeam()).remove(towerTile);
+                WorldView.getInstance().getScene().getLastChild().detachChild(TowerView.towerSpriteMap.get(towerTile.getTower()));
+            }
+        });
+    }
+    
     public static void setInstance(TowerController instance) {
         TowerController.instance = instance;
     }
